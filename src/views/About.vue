520f234447bcb7c153060a1fa0407249d18679ec
kwozniakowski <kwozniakowski99@gmail.com>
2020-12-31 00:57:08 +0100

    stworzenie projektu, sa juz pierwsze widoki

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-body">
+    <h1 id="h1"> Oto my </h1>
+    <div>Jesteśmy sklepem zajebistym, w którym można kupić świeże bułki, wędliny itd.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
